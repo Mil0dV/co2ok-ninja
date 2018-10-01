6c06a39dddbe024f0819972289bbf68a41990e07
Milo de Vries <intensmilo@gmail.com>
2018-10-01 09:30:58 +0200

    Switches redirects to http

--- src/background/on-installed.js
+++ src/background/on-installed.js
@@ -12,7 +12,7 @@ export function onInstalled() {
 
         //open welcome page for new installs
         if(details.reason == "install"){
-            chrome.tabs.create({url: "https://CO2ok.Ninja/welcome/"});
+            chrome.tabs.create({url: "https://co2ok.ninja/welcome/"});
         }
     });
 }
