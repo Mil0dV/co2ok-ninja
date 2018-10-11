f0a65d2010354acbc7c389a560cfcde463023467
assignon <kevineasky@gmail.com>
2018-10-11 11:18:31 +0200

    group test
    style aangepast zodat het bij elk patner site dezelf styling hebt

--- src/helpers/get-partners-list.js
+++ src/helpers/get-partners-list.js
@@ -3,11 +3,7 @@
  */
 export function getPartnersList(){
     var xhr = new XMLHttpRequest();
-<<<<<<< HEAD
-    xhr.open("GET", "https://co2ok.ninja/api/partners");
-=======
     xhr.open("GET", "https://co2ok.ninja/dojo/partners");
->>>>>>> 5e5482ddb1ea072353d7b5b5e974ab77e2585120
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4){
             if(xhr.responseText){
