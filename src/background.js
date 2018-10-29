cd478ce60ae825056b85c481562657eae367c18e
Michielv10hoven <m.v.tienhoven@gmail.com>
2018-10-29 11:17:42 +0100

    Added google form for when user Uninstalls
    When the user removes CO2ok Ninja from their browser they are now redirected to a google form with questions as of why

--- src/background.js
+++ src/background.js
@@ -12,3 +12,16 @@ deactivateMonetizing();
 
 import { recognizeOtherAffiliates } from './background/recognize-other-affiliates.js';
 recognizeOtherAffiliates();
+
+/* Check whether new version is installed */
+chrome.runtime.onInstalled.addListener(function(details) {
+    /* other 'reason's include 'update' */
+    if (details.reason == "install") {
+        /* If first install, set uninstall URL */
+        var uninstallGoogleFormLink = 'https://docs.google.com/forms/d/e/1FAIpQLScFzXABlaBzzKaiyCZR-REhBTC5rQVLaObU3dy6LcNr-awrgw/viewform';
+        /* If Browser version supports it... */
+        if (chrome.runtime.setUninstallURL) {
+            chrome.runtime.setUninstallURL(uninstallGoogleFormLink);
+        }
+    }
+});
